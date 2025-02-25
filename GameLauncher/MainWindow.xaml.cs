afeaad40465ebbdc7b55c9555a832f03a9cc9e5e
Levi <leventemajor36@gmail.com>
2025-02-25 11:21:01 +0100

    refactor

--- GameLauncher/MainWindow.xaml.cs
+++ GameLauncher/MainWindow.xaml.cs
@@ -14,11 +14,12 @@ namespace GameLauncher
 {
     public partial class MainWindow : Window
     {
-        private readonly ObservableCollection<Game> Games = new();
+        private ObservableCollection<Game> Games = new();
+        private ObservableCollection<ReviewClass> Reviews = new();
         private List<Game> Executables = new();
         private readonly string ImageDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DownloadedImages");
         private readonly string GameDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DownloadedGames");
-        private static string InstallationFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "installedGames.json");
+        private readonly string InstallationFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "installedGames.json");
         private const string SettingsFile = "user.settings";
         private const string ConnectionString = "Server=localhost;Database=game_launcher;Uid=root;Pwd=;";
         private readonly int userId;
@@ -33,7 +34,6 @@ namespace GameLauncher
                 if (_selectedGame != value)
                 {
                     _selectedGame = value;
-                    // Here you can trigger any updates related to the selected game
                     UpdateUIForSelectedGame();
                 }
             }
@@ -49,15 +49,13 @@ namespace GameLauncher
             GamesList.ItemsSource = Games;
         }
 
-        private void GameItem_Click(object sender, RoutedEventArgs e)
+        private void GamesListItem_Click(object sender, RoutedEventArgs e)
         {
             if (sender is Button button && button.DataContext is Game selectedGame)
             {
-                // Set the selected game, which will trigger the UI update
                 SelectedGame = selectedGame;
             }
         }
-
 
         private int GetUserId()
         {
@@ -283,7 +281,6 @@ namespace GameLauncher
                 }
             }
         }
-
 
         private void OnGameExit(string gameName, int gameID)
         {
@@ -527,12 +524,171 @@ namespace GameLauncher
             profileWindow.Show();
         }
 
+        private void ButtonChange_Click(object sender, RoutedEventArgs e)
+        {
+            lbxReviews.Visibility = lbxReviews.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            txbContent.Visibility = txbContent.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            txbTitle.Visibility = txbTitle.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            sldrRating.Visibility = sldrRating.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            lblRating.Visibility = lblRating.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            btnSubmit.Visibility = btnSubmit.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            lblRatingText.Visibility = lblRatingText.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+            btnChange.Content = btnChange.Content.Equals("Add Review") ? "Show Reviews" : "Add Review";
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = @"
+                        INSERT INTO reviews (game_id, user_id, rating, review_title, review_text)
+                        VALUES (@GameId, @UserId, @Rating, @Title, @Text)";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@GameId", SelectedGame.Id);
+                        cmd.Parameters.AddWithValue("@UserId", userId);
+                        cmd.Parameters.AddWithValue("@Rating", sldrRating.Value);
+                        cmd.Parameters.AddWithValue("@Title", txbTitle.Text);
+                        cmd.Parameters.AddWithValue("@Text", txbContent.Text);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Review submitted successfully");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Database error: {ex.Message}");
+            }
+        }
+
+        private void sldrRating_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (lblRating != null && sldrRating != null)
+            {
+                lblRating.Content = sldrRating.Value;
+            }
+        }
+
+        private ObservableCollection<ReviewClass> loadReviews(int currentGameId)
+        {
+            try
+            {
+                ObservableCollection<ReviewClass> reviews = new();
+
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string query = $"SELECT * FROM reviews WHERE game_id = '{currentGameId}';";
+
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int reviewUserId = reader.GetInt32("user_id");
+                                string reviewName = getName(reviewUserId);
+
+                                reviews.Add(new ReviewClass(
+                                    reviewName,
+                                    reader.GetInt32("rating"),
+                                    reader.GetString("review_title"),
+                                    reader.GetString("review_text")
+                                ));
+                            }
+                        }
+                    }
+                }
+                return reviews;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error while connecting to the database");
+                lblGameName.Content = $"Game ID : {currentGameId}";
+                throw;
+            }
+        }
+
+        private string getName(int reviewUserId)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    string query = $"SELECT name FROM users WHERE id = '{reviewUserId}';";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        return cmd.ExecuteScalar().ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error while connecting to the database");
+                return "Unknown";
+                throw;
+            }
+        }
+
+        private void ChangeLibraryVisibility(object sender, RoutedEventArgs e)
+        {
+            LibraryGrid.Visibility = LibraryGrid.Visibility.Equals(Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
+        }
+
         private void ReviewButton_Click(object sender, RoutedEventArgs e)
         {
             if (SelectedGame != null)
             {
-                Review review = new Review(SelectedGame.Id, userId);
-                review.Show();
+                    Reviews.Clear();
+                    var currentGameId = SelectedGame.Id;
+                    var currentUserId = userId;
+                    lblGameName.Visibility = Visibility.Visible;
+                    lbxReviews.Visibility = Visibility.Visible;
+                    btnChange.Visibility = Visibility.Visible;
+                    LibraryGrid.Visibility = Visibility.Hidden;
+
+                    InitializeComponent();
+
+                    this.DataContext = this;
+
+                    try
+                    {
+                        using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                        {
+                            connection.Open();
+                            string query = $"SELECT name FROM games WHERE id = '{currentGameId}';";
+
+                            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                            {
+                                lblGameName.Content = cmd.ExecuteScalar().ToString();
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error while connecting to the database");
+                        lblGameName.Content = $"Game ID : {currentGameId}";
+                        throw;
+                    }
+
+                    var loadedReviews = loadReviews(currentGameId);
+                    foreach (var review in loadedReviews)
+                    {
+                        Reviews.Add(review);
+                    }
+
+                    lbxReviews.ItemsSource = Reviews;
             }
             else
             {
